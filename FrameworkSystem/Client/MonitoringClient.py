3a9659e2fc0309207cb5ca35ec031f7f66ac592d
louisjdmartin <louis.jd.martin@gmail.com>
2018-08-09 16:32:17 +0200

    Separating IOLoop code from MonitoringClient

--- FrameworkSystem/Client/MonitoringClient.py
+++ FrameworkSystem/Client/MonitoringClient.py
@@ -9,7 +9,6 @@ import time
 import types
 import os
 
-import tornado.ioloop
 
 import DIRAC
 from DIRAC import gConfig, gLogger, S_OK, S_ERROR
@@ -55,30 +54,11 @@ class MonitoringFlusher( object ):
       self.__mcList.append( mc )
 
 
-class MonitoringFlusherTornado(object):
-  """
-  This class flushes all monitoring clients registered
-  Works with the Tornado IOLoop
-  """
 
-  def __init__(self):
-    self.__mcList = []
-    gLogger.info("Using MonitoringClient in IOLoop mode")
-    # Here we don't need to use IOLoop.current(), tornado will attach periodic callback to the current IOLoop himself
-    # We set callback every 5 minnutes
-    tornado.ioloop.PeriodicCallback(self.flush, 300000).start()
-
-  def flush(self, allData=False):
-    gLogger.info('Flushing monitoring')
-    for mc in self.__mcList:
-      mc.flush(allData)
-
-  def registerMonitoringClient(self, mc):
-    if mc not in self.__mcList:
-      self.__mcList.append(mc)
 
 # USE_TORNADO_IOLOOP is defined by starting scripts
 if os.environ.get('USE_TORNADO_IOLOOP', 'false').lower() == 'true':
+  from DIRAC.FrameworkSystem.Client.MonitoringClientIOLoop import MonitoringFlusherTornado
   gMonitoringFlusher = MonitoringFlusherTornado()
 else:
   gMonitoringFlusher = MonitoringFlusher()
