d79921fe2390acef8c17944a8d31c9d9bcf2094a
louisjdmartin <louis.jd.martin@gmail.com>
2018-08-13 16:28:05 +0200

    Removing code who needs Tornado

--- ConfigurationSystem/Client/ConfigurationServerClient.py
+++ ConfigurationSystem/Client/ConfigurationServerClient.py
@@ -5,59 +5,13 @@
   Because of limitation with JSON some datas are encoded in base64
 
   IMPORTANT NOTE: for now this is only a preparation for HTTPS, 
-  ConfigurationServerJSON is NOT used while tornado is not merge.
-  ConfigurationServerClient is used instead but httpsClient is also
-  not used for now
+  ConfigurationServerClient is used but httpsClient is not used for now
 
 """
 
 from base64 import b64encode, b64decode
 
-from DIRAC.TornadoServices.Client.TornadoClient import TornadoClient
 from DIRAC.Core.Base.Client import Client
-
-
-
-class ConfigurationServerJSON(TornadoClient):
-  """
-    The specific client for configuration system.
-    To avoid JSON limitation the HTTPS handler encode data in base64
-    before sending them, this class only decode the base64
-
-    An exception is made with CommitNewData wich ENCODE in base64
-  """
-
-
-  def getCompressedData(self):
-    """
-      Transmit request to service and get data in base64,
-      it decode base64 before returning
-
-      :returns str:Configuration data, compressed
-    """
-    retVal = self.executeRPC('getCompressedData')
-    if retVal['OK']:
-      retVal['Value'] = b64decode(retVal['Value'])
-    return retVal
-
-  def getCompressedDataIfNewer(self, sClientVersion):
-    """
-      Transmit request to service and get data in base64,
-      it decode base64 before returning.
-
-      :returns str:Configuration data, if changed, compressed
-    """
-    retVal = self.executeRPC('getCompressedDataIfNewer', sClientVersion)
-    if retVal['OK'] and 'data' in retVal['Value']:
-      retVal['Value']['data'] = b64decode(retVal['Value']['data'])
-    return retVal
-
-  def commitNewData(self, sData):
-    """
-      Transmit request to service by encoding data in base64.
-    """
-    return self.executeRPC('commitNewData', b64encode(sData))
-
 
 
 class ConfigurationServerClient(Client):
@@ -72,4 +26,4 @@ class ConfigurationServerClient(Client):
   """
 
   # The JSON decoder for Configuration Server
-  httpsClient = ConfigurationServerJSON
+  pass
